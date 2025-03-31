0049051bf79233522d4f0a48aa4d84ba8b9c6ad7
CedricL-hsg <cedric-j.lampert@bluewin.ch>
2025-03-31 14:29:18 +0200

    added things

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import streamlit as st
 
-st.write("Hoooray Hooray its holiday")
+st.title("📊 Dein persönlicher Anlageassistent")
+
+startkapital = st.text_input("Startkapital in CHF", "10000")
+
+einkommen = st.slider("Monatliches Einkommen (CHF)", 
+                      0,
+                      15000,
+                      5000,
+                      step=500
+                      )
+
+risikoprofil = st.selectbox("Risikoprofil", ["konservativ", "ausgewogen", "dynamisch"])
